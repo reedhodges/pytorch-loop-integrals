f4c45ac3f0ec1d8ed3131cb53f4e7303b4090ea2
Reed Hodges <61560859+reedhodges@users.noreply.github.com>
2024-03-12 15:30:48 -0400

    tried EfficientNet_B0

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import torch
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 from torch import nn, optim
+from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
+from torchvision.models._api import WeightsEnum
+from torch.hub import load_state_dict_from_url
 
 from pathlib import Path
 from PIL import Image
@@ -18,16 +21,16 @@ test_path = data_path / "test"
 
 image_path_list = list(data_path.glob("**/*.png"))
 
-def convert_to_rgb(x):
-    return x.convert('RGB')
+# this block is needed to fix an error with the weights download
+def get_state_dict(self, *args, **kwargs):
+    kwargs.pop("check_hash")
+    return load_state_dict_from_url(self.url, *args, **kwargs)
+WeightsEnum.get_state_dict = get_state_dict
 
-data_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.Lambda(convert_to_rgb),
-    transforms.RandomAffine(degrees=5, translate=(0.05, 0.05), scale=(0.95, 1.05)),
-    transforms.ToTensor(),
-    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
+weights = EfficientNet_B0_Weights.DEFAULT
+model = efficientnet_b0(weights=weights).to(device)
+
+data_transform = weights.transforms()
 
 train_dataset = datasets.ImageFolder(root=train_path, transform=data_transform)
 test_dataset = datasets.ImageFolder(root=test_path, transform=data_transform)
@@ -38,63 +41,91 @@ class_dict = train_dataset.class_to_idx
 BATCH_SIZE = 32
 NUM_WORKERS = 0 #os.cpu_count()
 
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers = NUM_WORKERS, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers = NUM_WORKERS, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
 
-class SimpleCNN(nn.Module):
-    def __init__(self, num_classes):
-        super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64 * 56 * 56, 128) 
-        self.fc2 = nn.Linear(128, num_classes)
-        self.relu = nn.ReLU()
-        self.pool = nn.MaxPool2d(2, 2)
-        self.dropout = nn.Dropout(0.5)
+for param in model.features.parameters():
+    param.requires_grad = False
 
-    def forward(self, x):
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)
-        x = self.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
+model.classifier = nn.Sequential(
+    nn.Dropout(p=0.2, inplace=True),
+    nn.Linear(in_features=1280,
+              out_features=len(class_names)).to(device)
+)
 
-num_classes = len(class_names)
-model = SimpleCNN(num_classes).to(device)
-
-criterion = nn.CrossEntropyLoss()
+loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-NUM_EPOCHS = 5
+NUM_EPOCHS = 3
 
 for epoch in tqdm(range(NUM_EPOCHS)):
+    train_loss = 0.0
+    train_correct = 0
+    train_total = 0
+
     model.train()
-    running_loss = 0.0
-    for inputs, labels in train_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        
+    for images, labels in train_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
         optimizer.zero_grad()
-        
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
+
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+
+        loss = loss_fn(outputs, labels)
         loss.backward()
         optimizer.step()
-        
-        running_loss += loss.item()
 
-    print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
+        train_loss += loss.item()
+        train_total += labels.size(0)
+        train_correct += (predicted == labels).sum().item()
 
-model.eval()
-correct = 0
-total = 0
-with torch.inference_mode():
-    for inputs, labels in test_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        outputs = model(inputs)
+    train_accuracy = 100 * train_correct / train_total
+
+    test_loss = 0.0
+    test_correct = 0
+    test_total = 0
+
+    model.eval()
+    with torch.inference_mode():
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+
+            loss = loss_fn(outputs, labels)
+
+            test_loss += loss.item()
+            test_total += labels.size(0)
+            test_correct += (predicted == labels).sum().item()
+
+    test_accuracy = 100 * test_correct / test_total
+
+    print(f"Epoch: {epoch+1}/{NUM_EPOCHS}")
+    print(f"Train Loss: {train_loss/train_total:.4f}")
+    print(f"Train Accuracy: {train_accuracy:.2f}%")
+    print(f"Test Loss: {test_loss/test_total:.4f}")
+    print(f"Test Accuracy: {test_accuracy:.2f}%")
+    print()
+
+torch.save(model.state_dict(), "models/efficientnet_b0.pth")
+
+# function to predict the class of an image
+def predict_class(image_path, model, class_dict, data_transform):
+    model.eval()
+    with torch.inference_mode():
+        image = Image.open(image_path)
+        image = data_transform(image)
+        image = image.unsqueeze(0)
+        image = image.to(device)
+        outputs = model(image)
         _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-print(f'Accuracy of the network on thme test images: {100 * correct / total} %')
+        class_idx = predicted.item()
+        class_name = list(class_dict.keys())[list(class_dict.values()).index(class_idx)]
+        return print(f"Predicted class: {class_name}")
+    
+# test the function
+predict_class("custom_images/IMG_5FAB788BE67E-1.png", model, class_dict, data_transform)
